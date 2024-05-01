afdb406232a3923d9cdfd2c709fdd253d58c524b
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-01 04:15:03 +0300

    File Entry class added - this class is to help import and export txt files in our virtual disk - it has three basic funcions 1- wrote 2-read 3-delete

---
+++ Cline/FileEntry.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cline
+{
+    internal class FileEntry : Directory_Entry
+    {
+        public Directory Parent = null;
+        public string content;
+
+        // constructor
+        public FileEntry(string name, byte attribute, int size, int starting_cluster, Directory Parent , string content)
+            : base(name, attribute, size, starting_cluster)
+        {
+            this.Parent = Parent;
+            this.content = content;
+        }
+
+        // function to convert content to bytes to serve writing to the disk
+        public byte[] ContentToBytes(string content)
+        {
+            byte[] ContentInBytes = Encoding.UTF8.GetBytes(content);
+            return ContentInBytes;
+        }
+
+        // function to convert bytes to string to serve reading from the disk
+        public string BytesToContent(byte[] content)
+        {
+            string ContentInString = Encoding.UTF8.GetString(content);
+            return ContentInString;
+        }
+
+        // function to write the file content to the disk
+        public void WriteFile()
+        {
+            byte[] contentInBytes = ContentToBytes(content);
+            List<byte[]> ReadyData = Directory.PrepData(contentInBytes);
+            int cluster = this.starting_cluster;
+            int i = 0;
+            while (i < ReadyData.Count)
+            {
+                Virtual_Disk.WriteBlock(ReadyData[i], cluster);
+                FatTable.SetVal(cluster, cluster + 1);
+                cluster = FatTable.First_Ava_Block();
+                i++;
+            }
+            FatTable.SetVal(cluster, -1);
+            FatTable.WriteFatTable();
+        }
+
+        // function to read the file content from the disk
+        public void ReadFile()
+        {
+            byte[] contentInBytes = new byte[this.size];
+            List<byte[]> ReadyData = new List<byte[]>(); // it will contain every block data , list of blocks
+            int cluster = this.starting_cluster;
+            int i = 0;
+            do
+            {
+                contentInBytes = Virtual_Disk.ReadBlock(cluster);
+                ReadyData.Add(contentInBytes);
+                cluster = FatTable.GetVal(cluster); // get the next clusetr that the data is stored in
+            }
+            while (cluster != -1); 
+            // convert list of bytes into one array of bytes then convert it into string using BytesToContent function
+            this.content = BytesToContent(ReadyData.SelectMany(a => a).ToArray());
+        }
+
+        // fuctnion to delete the file from the disk
+        public void DeleteFile()
+        {
+            int cluster = this.starting_cluster;
+            int nextCluster = FatTable.GetVal(cluster);
+            do
+            {
+                FatTable.SetVal(cluster, 0);
+                // i delete the data from the disk becaues override may make conflicts 
+                Virtual_Disk.WriteBlock(Virtual_Disk.EmptyBlock, cluster);
+                cluster = nextCluster;
+                nextCluster = FatTable.GetVal(cluster);
+            }
+            while (nextCluster != -1);
+            FatTable.SetVal(cluster, 0);
+
+            string FileName = new string(this.name);
+            int idx = this.Parent.SearchDir(FileName);
+            if(idx != -1)
+            {
+                this.Parent.DirectoryTable.RemoveAt(idx);
+                this.Parent.WriteDirectory();
+            }
+           
+            FatTable.WriteFatTable();
+        }
+    }
+}
